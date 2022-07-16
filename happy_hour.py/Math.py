c683baec76d089a552b7a78bd1c0ca25a5595645
Arjun Manoj Kumar <mkarjun2096@outlook.com>
2022-07-16 20:29:59 +0530

    4th

---
+++ happy_hour.py/Math.py
@@ -0,0 +1,14 @@
+# + plus
+# - minus
+# / divide
+# * times
+# ** exponentiation
+# % modulo
+# < less then
+# > greater then
+
+print("What is the answer to life, the universe, and everything?", int((40 + 30 - 7) * 2 / 3))
+print("Is 5*4 > 8*9 ?" )
+print(5*4 > 8*9)
+print (" 5 * 4 = ", 5 * 4 )
+print (" 8 * 9 = ", 8 * 9 )
