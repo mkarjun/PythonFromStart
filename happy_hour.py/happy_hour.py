a315aafbc63453d7fcda8e4ef170b2eb22c5babd
Arjun Manoj Kumar <arjun101096@gmail.com>
2022-07-16 15:57:16 +0530

    commit

--- happy_hour.py/happy_hour.py
+++ happy_hour.py/happy_hour.py
@@ -13,4 +13,4 @@ random_bar = random.choice(bars)
 random_person = random.choice(people)
 random_career = random.choice(career)
 
-print(f"you will {random_bar}  {random_person} and get a {random_career} career")
+print(f"You will {random_bar}  {random_person} \n and get a {random_career} career")
