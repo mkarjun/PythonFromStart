d485866ca21ecb53c5095320ad035fdd0745d48e
Arjun Manoj Kumar <mkarjun2096@outlook.com>
2022-07-16 21:51:07 +0530

    6th

---
+++ happy_hour.py/TipCalculator.py
@@ -0,0 +1,16 @@
+# Tip Calculator
+
+# This program will calculate the tip for a meal.
+# It will ask the user for the meal cost and the tip percentage.
+# It will then display the tip amount.
+# It will also display the total cost of the meal.
+# The program will replace the dollar sign with a blank space.
+
+print(input("Name: "))
+meal_cost = (input("Meal cost: "))
+meal_cost_dlr = float(meal_cost.replace("$", ""))
+tip_percentage = float(input(" Tip Percentage: "))
+tip_amount = meal_cost_dlr * ( tip_percentage / 100)
+total_cost = meal_cost_dlr + tip_amount
+print("Tip amount is: $", tip_amount)
+print("Total Cost is: $", total_cost)
