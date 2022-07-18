658b3c0402382b1e3a30d09407fb4a9099fc49c0
Arjun Manoj Kumar <mkarjun2096@outlook.com>
2022-07-18 23:09:05 +0530

    11th

---
+++ happy_hour.py/For.py
@@ -0,0 +1,12 @@
+the_count = [1, 2, 3, 4, 6]
+stocks = ["AAPL", "GOOG", "TSLA"]
+random_things = ["Puppies", 55, "Anthony Weiner", 1/2, ["Oh no", "A list inside a list"]]
+
+for number in the_count:
+    print(f"This is count {number}")
+
+for stock in stocks:
+    print(f"I own {stock}")
+
+for thing in random_things:
+    print(f"This is {thing}")
