6d421425638162471b6f2fbaa2048bd42b8f8947
Arjun Manoj Kumar <mkarjun2096@outlook.com>
2022-07-18 21:59:49 +0530

    elif added

--- happy_hour.py/Ifcondition.py
+++ happy_hour.py/Ifcondition.py
@@ -2,5 +2,7 @@ answer = input(print("Do you want to hear a joke?"))
 if answer == "yes" or answer == "Yes":
     print("Why did the chicken cross the road?")
     print("To get to the other side!")
+elif answer == "No":
+    print("Fine.")
 else:
     print("-\('-')/-")
