1c429aa0c3f4cbecea9717f734b5bc3728ec941f
Arjun Manoj Kumar <mkarjun2096@outlook.com>
2022-07-18 21:51:51 +0530

    7th

---
+++ happy_hour.py/Ifcondition.py
@@ -0,0 +1,6 @@
+answer = input(print("Do you want to hear a joke?"))
+if answer == "yes" or answer == "Yes":
+    print("Why did the chicken cross the road?")
+    print("To get to the other side!")
+else:
+    print("-\('-')/-")
