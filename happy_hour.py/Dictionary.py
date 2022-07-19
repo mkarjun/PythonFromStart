e2193b8d58dcc6f7200c5f32d410bc505f58b545
Arjun Manoj Kumar <mkarjun2096@outlook.com>
2022-07-19 14:59:24 +0530

    update Retreiving using .get

---
+++ happy_hour.py/Dictionary.py
@@ -0,0 +1,17 @@
+
+states = {'NY': 'New York', 'PA': 'Pennsylvania', 'CA': 'California'}
+
+print(states['NY'])
+
+print(states.get('NY','not found'))
+print(states.get('OG','not found'))
+
+arjun = {'Name':'Arjun','Height':'180'}
+aparna = {'Name':'Aparna','Height':'165'}
+shafeeq = {'Name':'Shafeeq','Height':'170'}
+
+name=[arjun,aparna,shafeeq]
+print(name)
+
+for person in name :
+    print(person.get('Height'))
