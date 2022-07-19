d2499869dfe84f0bea50897874e17855d5dbce0a
Arjun Manoj Kumar <mkarjun2096@outlook.com>
2022-07-19 13:57:02 +0530

    13th

---
+++ happy_hour.py/pullOutOfList.py
@@ -0,0 +1,9 @@
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+first_animal = animals[0]
+print(first_animal)
+third_animal = animals[2]
+print(third_animal)
+
+print("They're are this many things:", len(animals))
+another_list = animals[-1]
+print(another_list)
